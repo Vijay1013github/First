2d87fcf0dbd9be0b4226e31c9a19c08fca25e91e
C V Kasture <C V Kasture@Lenovo-PC>
2022-11-04 15:36:20 +0530

    Commit1

---
+++ Github1/src/Github2/Test1.java
@@ -0,0 +1,23 @@
+package Github2;
+
+import java.util.concurrent.TimeUnit;
+
+
+
+public class Test1 
+{
+	public static void main(String[] args) {
+		
+	
+	{
+	
+	System.out.println("Hello");
+		
+		
+
+		
+		
+	}
+		
+	}
+}
